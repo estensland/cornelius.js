ecf165f35c704296ec87bd99024c9c40ef6a2789
Eric Stensland <estensland88@gmail.com>
2014-09-13 08:52:56 -0500

    Basic approach

--- cornelius.js
+++ cornelius.js
@@ -1,2 +1,14 @@
-
-console.log('capitalism');
+window.cornelius = (function(){
+  console.log('capitalism');
+  
+  var cornelius = {
+    createTag: function(tag, content){
+      hTag = tag + ">"
+      return "<" + hTag + content + "</" + hTag;
+    },
+    
+    h: function(num, content){
+      return this.createTag('h'+num, content)
+    }
+  }
+}());
