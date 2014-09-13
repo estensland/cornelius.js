bddf16643c18627174ddff70ac7af53818fcc48d
Eric Stensland <estensland88@gmail.com>
2014-09-13 11:23:45 -0500

    Basic set up for creating string versions of tags with attributes and content. Nothing for nested yet. Also imported some basic each, map, select, and reject functions from another bit of code I have done

--- cornelius.js
+++ cornelius.js
@@ -1,14 +1,85 @@
 window.cornelius = (function(){
-  console.log('capitalism');
-  
-  var cornelius = {
-    createTag: function(tag, content){
-      hTag = tag + ">"
-      return "<" + hTag + content + "</" + hTag;
+  var corHelpiers = {
+    // These are adapted from github.com/estensland/MeadHall/Heorot-JS
+    each: function(input, func){
+      if (input instanceof Array){
+        for (var i=0, ii = input.length; i < ii; i ++) {
+          func(input[i]);
+        }
+      }
+      else if (input instanceof Object){
+        for (var key in input) {
+          if (input.hasOwnProperty(key) && key !== 'heorotKind') {
+            func(key, input[key]);
+          }
+        }
+      }
+      return input;
     },
-    
-    h: function(num, content){
-      return this.createTag('h'+num, content)
+
+    map: function(input, func){
+      returnArray = [];
+
+      if (input instanceof Array){
+        for (var i=0, ii = input.length; i < ii; i ++) {
+          returnArray.push(func(input[i]));
+        }
+      }
+      else if (input instanceof Object){
+        for (var key in input) {
+          if (input.hasOwnProperty(key) && key !== 'heorotKind') {
+            returnArray.push(func(key, input[key]));
+          }
+        }
+      }
+      return returnArray;
+    },
+
+    select: function(input, func){
+      returnArray = [];
+      for (var i=0, ii = input.length; i < ii; i ++) {
+        if (func(input[i])){
+          returnArray.push(input[i]);
+        }
+      }
+      return returnArray;
+    },
+
+    reject: function(input, func){
+      returnArray = [];
+      for (var i=0, ii = input.length; i < ii; i ++) {
+        if (!func(input[i])){
+          returnArray.push(input[i]);
+        }
+      }
+      return returnArray;
     }
-  }
+  };
+
+  var cornelius = {
+    createTag: function(tag, content, options){
+      attributes = corHelpiers.map(options, function(key, value){
+        return key + "='" + value +"'";
+      });
+
+      var openTag = "<" + tag;
+      if (attributes.length > 1){
+        openTag = openTag + " " + attributes.join(' ') + ">";
+      }
+      else{
+        openTag = openTag + ">";
+      }
+      return openTag + content + "</" + tag + '>';
+    },
+
+    h: function(num, content){
+      return this.createTag('h'+num, content, options);
+    },
+
+    div: function(num, content, options){
+      return this.createTag('h'+num, content, options);
+    },
+  };
+
+  return cornelius;
 }());
